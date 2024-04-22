d813ec26fb756f3a03914a0e4337f1562e3f99f9
Tiago THermes <101218962+TMHermes@users.noreply.github.com>
2024-04-22 15:43:33 -0300

    iniciando criptografia de senhas

--- pags/api/login.ts
+++ pags/api/login.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import connection from '../../middlewares/conectarMySQL';
+import bcrypt from 'bcrypt';
 
 const router = express.Router();
 
@@ -8,16 +9,25 @@ router.post('/login', async (req, res) => {
 
     connection.connect();
 
-    const query = `SELECT * FROM usuarios WHERE email = '${email}' AND senha = '${senha}'`;
-    connection.query(query, (error, resposta) => {
+    const query = `SELECT * FROM usuarios WHERE email = ?`;
+    connection.query(query, [email], async (error, resposta) => {
         if (error) {
             res.status(500).json({ message: 'Erro ao consultar o banco de dados' });
-        } else {
-            if (resposta.length > 0) {
-                res.status(200).json({ message: 'Login  realizado com sucesso!'});
-            } else {
+            return;
+        }
+
+        if (resposta.length === 0) {
                 res.status(401).json({ message: 'Credenciais inválidas.' });
+                return;
             }
+
+        const usuario = resposta[0];
+        const senhaCorreta = await bcrypt.compare(senha, usuario.senha);
+
+        if (senhaCorreta) {
+            res.status(200).json({ message: 'Login realizado com sucesso!' });
+        } else {
+            res.status(401).json({ message: 'Credenciais inválidas.' })
         }
     });
 
